f774f4b6d0b981894f8f33dfb340811ab9598237
ptrvsrg <s.petrov1@g.nsu.ru>
2022-09-12 14:08:12 +0700

    Remove #pragma

--- lab2/src/monte_carlo/monte_carlo.cpp
+++ lab2/src/monte_carlo/monte_carlo.cpp
@@ -1,28 +1,5 @@
 #include "monte_carlo.h"
 
-#pragma GCC push_options
-#ifdef O0
-    #pragma GCC optimize("O0")
-#endif // O0
-#ifdef O1
-    #pragma GCC optimize("O1")
-#endif // O1
-#ifdef O2
-    #pragma GCC optimize("O2")
-#endif // O2
-#ifdef O3
-    #pragma GCC optimize("O3")
-#endif // O3
-#ifdef Os
-    #pragma GCC optimize("Os")
-#endif // Os
-#ifdef Ofast
-    #pragma GCC optimize("Ofast")
-#endif // Ofast
-#ifdef Og
-    #pragma GCC optimize("Og")
-#endif // Og
-
 double MonteCarloAlgorithm(long long count)
 {
     InitRand();
@@ -39,5 +16,3 @@ double MonteCarloAlgorithm(long long count)
 
     return insideCount / count;
 }
-
-#pragma GCC pop_options
