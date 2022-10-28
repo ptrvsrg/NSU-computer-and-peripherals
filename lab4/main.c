cf8ffd77db7ecca581b9d7a5d3c580b70aea87fc
ptrvsrg <s.petrov1@g.nsu.ru>
2022-10-28 21:03:55 +0700

    Add lab code

---
+++ lab4/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+double MonteCarloAlgorithm(int count)
+{
+    double insideCount = 0.0;
+    srand(time(NULL));
+
+    for (int i = 0; i < count; ++i)
+    {
+        double x = (double)rand() / RAND_MAX;
+        double y = (double)rand() / RAND_MAX;
+
+        if ((x * x) + (y * y) <= 1.0)
+        {
+            insideCount += 4.0;
+        }
+    }
+
+    return insideCount / count;
+}
+
+int main()
+{
+    int count = 100000000;
+
+    double pi = MonteCarloAlgorithm(count);
+    printf("PI: %lf\n", pi);
+
+    return EXIT_SUCCESS;
+}
