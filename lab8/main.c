4c52ccd3cabbccfab8ede80c60bddfba7ea84019
ptrvsrg <s.petrov1@g.nsu.ru>
2022-11-30 22:53:04 +0700

    Add C program

--- lab8/main.c
+++ lab8/main.c
@@ -1,20 +1,21 @@
-#include <cstdint>
-#include <cstdlib>
-#include <ctime>
-#include <iomanip>
-#include <iostream>
-#include <set>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 #define MIN_SIZE (1 * 1024)
 #define MAX_SIZE (2 * 1024 * 1024)
 
-using Time = uint64_t;
+typedef uint64_t Time;
 
-void Clock(Time & time);
+void Clock(Time * time);
 void CreateStraight(uint32_t * array,
                     uint32_t size);
 void CreateReverse(uint32_t * array,
                    uint32_t size);
+uint32_t FindIndex(uint32_t * indexes,
+                   uint32_t ind);
 void CreateRandom(uint32_t * array,
                   uint32_t size);
 uint64_t CalcTime(const uint32_t * array,
@@ -22,40 +23,40 @@ uint64_t CalcTime(const uint32_t * array,
 void Bypass(const uint32_t * array,
             uint32_t size);
 
-int main()
+int main(void)
 {
-    std::cout << std::setw(10) << std::left << "Size"
-              << std::setw(10) << std::left << "Random"
-              << std::setw(10) << std::left << "Reverse"
-              << std::setw(10) << std::left << "Straight" << std::endl;
-
     for (uint32_t size = MIN_SIZE; size <= MAX_SIZE; size *= 2)
     {
-        std::cout << std::setw(10) << std::left << size * sizeof(uint32_t);
+        printf("Size: %lu\n", size * sizeof(uint32_t));
 
-        auto * array = new uint32_t[size];
-
-        CreateRandom(array,
-                     size);
-        std::cout << std::setw(10) << std::left << CalcTime(array, size);
-
-        CreateReverse(array,
-                      size);
-        std::cout << std::setw(10) << std::left << CalcTime(array, size);
+        uint32_t * array = malloc(size * sizeof(uint32_t));
 
         CreateStraight(array,
                        size);
-        std::cout << std::setw(10) << std::left << CalcTime(array, size) << std::endl;
+        Bypass(array,
+               size);
 
-        delete [] array;
+        CreateRandom(array,
+                     size);
+        printf("Random: %lu\t", CalcTime(array, size));
+
+        CreateReverse(array,
+                      size);
+        printf("Reverse: %lu\t", CalcTime(array, size));
+
+        CreateStraight(array,
+                       size);
+        printf("Straight: %lu\n", CalcTime(array, size));
+
+        free(array);
     }
 
     return EXIT_SUCCESS;
 }
 
-void Clock(Time & time)
+void Clock(Time * time)
 {
-    asm("rdtsc\n":"=a"(time));
+    asm("rdtsc\n":"=a"(*time));
 }
 
 void CreateStraight(uint32_t * array,
@@ -74,23 +75,45 @@ void CreateReverse(uint32_t * array,
         array[i] = i - 1;
 }
 
+uint32_t FindIndex(uint32_t * indexes,
+                   uint32_t ind)
+{
+    for (int i = 0, j = 0; ; ++i)
+    {
+        if (indexes[i] == -1)
+        {
+            continue;
+        }
+
+        if (j == ind)
+        {
+            uint32_t res = indexes[i];
+            indexes[i] = -1;
+            return res;
+        }
+
+        ++j;
+    }
+}
+
 void CreateRandom(uint32_t * array,
                   uint32_t size)
 {
-    std::set<uint32_t> indexes;
+    uint32_t * indexes = malloc((size - 1) * sizeof(uint32_t));
     for (uint32_t i = 0; i < size - 1; ++i)
-        indexes.insert(i + 1);
+        indexes[i] = i + 1;
 
-    srandom(time(nullptr));
+    srandom(time(NULL));
     uint32_t ind = 0;
-    while (!indexes.empty())
+    uint32_t indexes_size = size - 1;
+    while (indexes_size != 0)
     {
-        uint32_t indexes_ind = random() % indexes.size();
-        auto next_ind_iter = std::next(indexes.begin(),
-                                       indexes_ind);
-        array[ind] = *next_ind_iter;
-        ind = *next_ind_iter;
-        indexes.erase(next_ind_iter);
+        uint32_t indexes_ind = random() % indexes_size;
+        uint32_t next_ind = FindIndex(indexes,
+                                      indexes_ind);
+        array[ind] = next_ind;
+        ind = next_ind;
+        --indexes_size;
     }
 
     array[ind] = 0;
@@ -107,14 +130,14 @@ uint64_t CalcTime(const uint32_t * array,
                   uint32_t size)
 {
     uint64_t min = UINT64_MAX;
-    Time start{}, end{};
+    Time start, end;
 
     for (int i = 0; i < 3; ++i)
     {
-        Clock(start);
+        Clock(&start);
         Bypass(array,
                size);
-        Clock(end);
+        Clock(&end);
 
         uint64_t res = (end - start) / size;
         min = (min < res) ? min : res;
