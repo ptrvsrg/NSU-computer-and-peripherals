b9c511f5d57f51e8e3271e091ac39ee65d6d091e
ptrvsrg <s.petrov1@g.nsu.ru>
2022-12-20 00:04:18 +0700

    Delete PAPI; Use Perf

--- lab10/BTB_benchmark/create_branches.c
+++ lab10/BTB_benchmark/create_branches.c
@@ -1,9 +1,10 @@
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(int argc, char ** argv)
 {
-    if (argc < 2)
+    if (argc < 3)
     {
         fprintf(stderr, "Few args error\n");
         return EXIT_FAILURE;
@@ -16,24 +17,36 @@ int main(int argc, char ** argv)
         return EXIT_FAILURE;
     }
 
-    int branch_count = atoi(argv[1]);
+    int count_branches = atoi(argv[1]);
+    int offset = atoi(argv[2]);
+    if (offset < 2)
+        offset = 2;
+    int power = log2(offset);
 
     // write header
     fprintf(file,
-        "#ifndef BRANCHES_H\n"
-        "#define BRANCHES_H\n\n"
-        "void RunBranches(void) {\n"
-        "\tasm volatile (\n"
-        "\t\t\"mov $10, %%eax;\\n\"\n"
-        "\t\t\"cmp $15, %%eax;\\n\"\n"
-        "\t\t\".p2align 1\\n\"\n"
+            "#ifndef BRANCHES_H\n"
+            "#define BRANCHES_H\n\n"
+            "void RunBranches(void) {\n"
+            "\tasm volatile (\n"
+            "\t\t\"mov $10, %%eax;\\n\"\n"
+            "\t\t\"cmp $15, %%eax;\\n\"\n"
+            "\t\t\".p2align %d;\\n\"\n",
+            power
     );
 
-    for (int i = 0; i < branch_count; ++i)
+    for (int i = 0; i < count_branches; ++i)
+    {
         fprintf(file,
                 "\t\t\"jle label_%d;\\n\"\n"
-                "\t\t\"label_%d:\\n\"\n",
-                i, i);
+                "\t\t\"nop;\\n\"\n",
+                i);
+
+        for (int j = 0; j < offset - 2; ++j)
+            fprintf(file, "\t\t\"nop;\\n\"\n");
+
+        fprintf(file, "\t\t\"label_%d:\\n\"\n", i);
+    }
 
     // write tail
     fprintf(file,
@@ -43,5 +56,6 @@ int main(int argc, char ** argv)
     );
 
     fclose(file);
+
     return EXIT_SUCCESS;
 }
--- lab10/BTB_benchmark/main.c
+++ lab10/BTB_benchmark/main.c
@@ -2,42 +2,11 @@
 #include <stdlib.h>
 
 #include "branches.h"
-#include "papi.h"
 
 int main()
 {
-    int retval = PAPI_library_init(PAPI_VER_CURRENT);
-    if (retval != PAPI_VER_CURRENT)
-    {
-        fprintf(stderr, "%s\n", PAPI_strerror(retval));
-        return EXIT_FAILURE;
-    }
-
-    int events[] =
-    {
-        PAPI_BR_MSP,    // Conditional branch instructions mispredicted
-        PAPI_BR_INS     // Total branch instructions executed
-    };
-    long long values[2];
-
-    retval = PAPI_start_counters(events, sizeof(events) / sizeof(*events));
-    if (retval != PAPI_OK)
-    {
-        fprintf(stderr, "%s\n", PAPI_strerror(retval));
-        return EXIT_FAILURE;
-    }
-
     for (int i = 0; i < 1000000; ++i)
         RunBranches();
-
-    retval = PAPI_stop_counters(values, sizeof(values) / sizeof(*values));
-    if (retval != PAPI_OK)
-    {
-        fprintf(stderr, "%s\n", PAPI_strerror(retval));
-        return EXIT_FAILURE;
-    }
-
-    printf("%lf", (double)values[0] * 100 / (double)values[1]);
 
     return EXIT_SUCCESS;
 }
